4faee7c10762b1030e1955e6db0914fa998291ab
Philip Mitchell Torrance II <ptorrance@Philips-MacBook-Air.local>
2017-07-31 16:55:42 -0700

    cleaned up code

--- reactApp/Container.js
+++ reactApp/Container.js
@@ -1,34 +1,33 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 import WidgetContainer from './components/WidgetContainer';
 
- class Container extends React.Component {
-   constructor() {
-     super();
-     this.state = {
-       isActive: false
-     }
+class Container extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isActive: false
+    };
 
-     this.isMirrorActive = this.isMirrorActive.bind(this);
-   }
+    this.isMirrorActive = this.isMirrorActive.bind(this);
+  }
 
-   isMirrorActive() {
+  isMirrorActive() {
      // function passed down to voice component to determine if
      // mirror is standby or active
-     return
-   }
+    return;
+  }
 
-   render () {
-       return(
-           <WidgetContainer />
-       )
+  render () {
+    return(
+        <WidgetContainer />
+    );
      // in future, have either just header Component
 
      // or the widgetContainer, header, and voice components
 
 
-   }
- }
+  }
+}
 
- export default Container;
+export default Container;
