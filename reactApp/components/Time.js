f0f6037d04db6107ab41a77cf684cc61cb2c7b59
Philip Mitchell Torrance II <ptorrance@Philips-MacBook-Air.local>
2017-07-31 14:55:57 -0700

    cleaned up Time

--- reactApp/components/Time.js
+++ reactApp/components/Time.js
@@ -1,30 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 class Time extends React.Component {
   constructor() {
-     super();
-     this.state = {
-       hours: new Date().getHours(),
-       minutes: new Date().getMinutes(),
-       seconds: new Date().getSeconds(),
-       interval: () => ''
-     };
-   }
-componentDidMount() {
-  //update time
-  this.setState({ interval: setInterval(() => {
-    this.setState({ hours: new Date().getHours(),
-                    minutes: new Date().getMinutes(),
-                    seconds: new Date().getSeconds()
-                  })
-                }, 100)
-              });
+    super();
+    this.state = {
+      hours: new Date().getHours(),
+      minutes: new Date().getMinutes(),
+      seconds: new Date().getSeconds(),
+      interval: () => ''
+    };
   }
-componentWillUnmount() {
-  clear(this.state.interval);
-  console.log(clearing);
-}
+  componentDidMount() {
+  //update time
+    this.setState({ interval: setInterval(() => {
+      this.setState({
+        hours: new Date().getHours(),
+        minutes: new Date().getMinutes(),
+        seconds: new Date().getSeconds()
+      });
+    }, 100)
+    });
+  }
+  componentWillUnmount() {
+    clearInterval(this.state.interval);
+    console.log('clearing');
+  }
   render() {
     return (
       <div className='time'>
