155be09675b515fa7fb653c2134136e3bde2e606
Amanda <ajhwhiz@gmail.com>
2017-08-02 16:06:19 -0700

    currently leaving speech call commented out

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,9 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-// YO AMANDA IS TRYING TO RUN LISTEN
-const { streamingMicRecognize } = require('./stt/listen');
-streamingMicRecognize();
+// // CURRENTLY THIS IS WHERE WE CAN START LISTENING FOR ONE COMMAND
+// const { streamingMicRecognize } = require('./stt/listen');
+// streamingMicRecognize();
 
 app.listen(3000, function () {
   console.log('Backend server for Electron App running on port 3000!')
