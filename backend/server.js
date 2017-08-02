52c5888dac4a3e2dfaa3daa75e6436e3e52bee17
Amanda <ajhwhiz@gmail.com>
2017-08-02 15:54:45 -0700

    SPEECH IS WORKING IN SERVER :)

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,10 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
+// YO AMANDA IS TRYING TO RUN LISTEN
+const { streamingMicRecognize } = require('./stt/listen');
+streamingMicRecognize();
+
 app.listen(3000, function () {
   console.log('Backend server for Electron App running on port 3000!')
 })
